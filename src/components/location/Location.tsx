247165ec634c2c11ed19adefebc042d1e456fced
Karl Marx-Levi <kmarx-levi@smc3.com>
2023-12-25 07:41:53 -0500

    add location

---
+++ src/components/location/Location.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import  { Navigate } from 'react-router-dom'
+
+const ProtectedComponent = () => {
+
+        return <Navigate  to='https://maps.app.goo.gl/PqU2XyAgU3kZPM6C6'  />
+}
